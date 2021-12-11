c73bf900e55729bc3758eb44254394f1fb02cefd
manbow1206 <tyasunaga@iyell.jp>
2021-12-11 17:45:08 +0900

    main page

--- components/Auth.js
+++ components/Auth.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { Cookie } from 'universal-cookie';
-import { LockClosedIcon } from '@heroicons/react/solid';
+import Cookie from 'universal-cookie';
 
 const cookie = new Cookie();
 
@@ -9,20 +8,23 @@ export default function Auth() {
   const router = useRouter();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isLogin, setIsLogin] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
 
   const login = async () => {
     try {
-      await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create`, {
-        method: 'POST',
-        body: JSON.stringify({ username: username, password: password }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
+      await fetch(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+        {
+          method: 'POST',
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
         .then((res) => {
           if (res.status === 400) {
-            throw 'authentication faild';
+            throw 'authentication failed';
           } else if (res.ok) {
             return res.json();
           }
@@ -43,7 +45,7 @@ export default function Auth() {
       login();
     } else {
       try {
-        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register`, {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
           method: 'POST',
           body: JSON.stringify({ username: username, password: password }),
           headers: {
@@ -51,9 +53,7 @@ export default function Auth() {
           },
         }).then((res) => {
           if (res.status === 400) {
-            throw 'authentication faild';
-          } else if (res.ok) {
-            return res.json();
+            throw 'authentication failed';
           }
         });
         login();
@@ -64,75 +64,85 @@ export default function Auth() {
   };
 
   return (
-    <>
-      <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-        <div className='max-w-md w-full space-y-8'>
-          <div>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              className='mx-auto h-12 w-auto'
-              src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
-              alt='Workflow'
-            />
-            <h2 className='mt-6 text-center text-3xl font-extrabold text-white'>
-              Sign in to your account
-            </h2>
-          </div>
-          <form className='mt-8 space-y-6' action='#' method='POST'>
-            <input type='hidden' name='remember' defaultValue='true' />
-            <div className='rounded-md shadow-sm -space-y-px'>
-              <div>
-                <input
-                  id='email-address'
-                  name='email'
-                  type='email'
-                  autoComplete='email'
-                  required
-                  className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                  placeholder='Email address'
-                />
-              </div>
-              <div>
-                <input
-                  id='password'
-                  name='password'
-                  type='password'
-                  autoComplete='current-password'
-                  required
-                  className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                  placeholder='Password'
-                />
-              </div>
-            </div>
-
-            <div className='flex items-center justify-center'>
-              <div className='text-sm'>
-                <span
-                  href='#'
-                  className='cursor-pointer font-medium text-white hover:text-indigo-500'
-                >
-                  change mode ?
-                </span>
-              </div>
-            </div>
-
-            <div>
-              <button
-                type='submit'
-                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-              >
-                <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
-                  <LockClosedIcon
-                    className='h-5 w-5 text-indigo-500 group-hover:text-indigo-400'
-                    aria-hidden='true'
-                  />
-                </span>
-                Sign in
-              </button>
-            </div>
-          </form>
-        </div>
+    <div className='max-w-md w-full space-y-8'>
+      <div>
+        <img
+          className='mx-auto h-12 w-auto'
+          src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
+          alt='Workflow'
+        />
+        <h2 className='mt-6 text-center text-3xl font-extrabold text-white'>
+          {isLogin ? 'Login' : 'Sign up'}
+        </h2>
       </div>
-    </>
+      <form className='mt-8 space-y-6' onSubmit={authUser}>
+        <input type='hidden' name='remember' value='true' />
+        <div className='rounded-md shadow-sm -space-y-px'>
+          <div>
+            <input
+              name='username'
+              type='text'
+              autoComplete='username'
+              required
+              className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+              placeholder='Username'
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <input
+              name='password'
+              type='password'
+              autoComplete='current-password'
+              required
+              className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+
+        <div className='flex items-center justify-center'>
+          <div className='text-sm'>
+            <span
+              onClick={() => setIsLogin(!isLogin)}
+              className='cursor-pointer font-medium text-white hover:text-indigo-500'
+            >
+              change mode ?
+            </span>
+          </div>
+        </div>
+
+        <div>
+          <button
+            type='submit'
+            className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+          >
+            <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
+              <svg
+                className='h-5 w-5 text-indigo-500 group-hover:text-indigo-400'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                aria-hidden='true'
+              >
+                <path
+                  fillRule='evenodd'
+                  d='M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z'
+                  clipRule='evenodd'
+                />
+              </svg>
+            </span>
+            {isLogin ? 'Login with JWT' : 'Create new user'}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }

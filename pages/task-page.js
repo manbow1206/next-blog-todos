55cf797413f8bfb35722ad5e6ad5784ffa89bf0c
manbow1206 <tyasunaga@iyell.jp>
2021-12-12 16:08:14 +0900

    last commit

--- pages/task-page.js
+++ pages/task-page.js
@@ -5,7 +5,6 @@ import { getAllTasksData } from '../lib/tasks';
 import Task from '../components/Task';
 import useSWR from 'swr';
 import StateContextProvider from '../context/StateContext';
-
 import TaskForm from '../components/TaskForm';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -13,52 +12,47 @@ const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
 
 export default function TaskPage({ staticfilterdTasks }) {
   const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
-    fallbackData: staticfilterdTasks,
+    initialData: staticfilterdTasks,
   });
-
-  const filterdTasks = tasks?.sort(
+  const filteredTasks = tasks?.sort(
     (a, b) => new Date(b.created_at) - new Date(a.created_at)
   );
-
   useEffect(() => {
     mutate();
   }, []);
-
   return (
     <StateContextProvider>
-      <Layout title='Task page '>
+      <Layout title='Task page'>
         <TaskForm taskCreated={mutate} />
         <ul>
-          {filterdTasks &&
-            filterdTasks.map((task) => (
+          {filteredTasks &&
+            filteredTasks.map((task) => (
               <Task key={task.id} task={task} taskDeleted={mutate} />
             ))}
         </ul>
         <Link href='/main-page' passHref>
           <div className='flex cursor-pointer mt-12'>
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-6 w-6 mr-3'
+              className='w-6 h-6 mr-3'
               fill='none'
-              viewB
-              ox='0 0 24 24'
               stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
             >
               <path
                 strokeLinecap='round'
                 strokeLinejoin='round'
-                strokeWidth='2'
+                strokeWidth={2}
                 d='M11 19l-7-7 7-7m8 14l-7-7 7-7'
               />
             </svg>
-            <span>Back to page</span>
+            <span>Back to main page</span>
           </div>
         </Link>
       </Layout>
     </StateContextProvider>
   );
 }
-
 export async function getStaticProps() {
   const staticfilterdTasks = await getAllTasksData();
 

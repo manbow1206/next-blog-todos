d75d94df35ff9c1d660db37fc37cf155611db536
manbow1206 <tyasunaga@iyell.jp>
2021-12-12 15:31:20 +0900

    add task edit button

---
+++ context/StateContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from 'react';
+
+export const StateContext = createContext();
+
+export default function StateContextProvider(props) {
+  const [selectedTask, setSelectedTaskd] = useState({ id: 0, title: '' });
+  return (
+    <StateContext.Provider
+      value={{
+        selectedTask,
+        setSelectedTaskd,
+      }}
+    >
+      {props.children}
+    </StateContext.Provider>
+  );
+}
--- pages/task-page.js
+++ pages/task-page.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { getAllTasksData } from '../lib/tasks';
 import Task from '../components/Task';
 import useSWR from 'swr';
+import StateContextProvider from '../context/StateContext';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
@@ -22,32 +23,36 @@ export default function TaskPage({ staticfilterdTasks }) {
   }, []);
 
   return (
-    <Layout title='Task page '>
-      <ul>
-        {filterdTasks &&
-          filterdTasks.map((task) => <Task key={task.id} task={task} taskDeleted={mutate}/>)}
-      </ul>
-      <Link href='/main-page' passHref>
-        <div className='flex cursor-pointer mt-12'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6 mr-3'
-            fill='none'
-            viewB
-            ox='0 0 24 24'
-            stroke='currentColor'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M11 19l-7-7 7-7m8 14l-7-7 7-7'
-            />
-          </svg>
-          <span>Back to page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title='Task page '>
+        <ul>
+          {filterdTasks &&
+            filterdTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href='/main-page' passHref>
+          <div className='flex cursor-pointer mt-12'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6 mr-3'
+              fill='none'
+              viewB
+              ox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M11 19l-7-7 7-7m8 14l-7-7 7-7'
+              />
+            </svg>
+            <span>Back to page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 
